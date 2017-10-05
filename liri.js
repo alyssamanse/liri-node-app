99c82ee9fa62c1fb848f0482d87c5beca00bd637
Alyssa Manse <alyssa.manse@austin.utexas.edu>
2017-10-05 17:25:08 -0500

    all but do-what-it-says

--- liri.js
+++ liri.js
@@ -13,8 +13,6 @@ var spotify = new Spotify({
   secret: spotifyClientSecret
 });
 
-var omdbQueryUrl = "http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=40e9cece";
-
 var nodeArgs = process.argv;
 var action = process.argv[2];
 var query = "";
@@ -43,27 +41,51 @@ function myTweets() {
 }
 
 function spotifyThisSong(query) {
-	spotify.search({ type: 'track', query: query }, function(error, data) {
-		if (error) {
-			return console.log('Error occurred: ' + err);
-		}
 
-		console.log("Artist: " + data.tracks.items[0].artists[0].name);
-		console.log("Song Name: " + data.tracks.items[0].name) 
-		console.log("Preview Link: " + data.tracks.items[0].album.external_urls.spotify)
-		console.log("Album: " + data.tracks.items[0].album.name) 
-	});
+	if (query === "") {
+		spotify.search({ type: 'track', query: "ace+of+base" }, function(error, data) {
+			console.log("Artist: " + data.tracks.items[0].artists[0].name);
+			console.log("Song Name: " + data.tracks.items[0].name) 
+			console.log("Preview Link: " + data.tracks.items[0].album.external_urls.spotify)
+			console.log("Album: " + data.tracks.items[0].album.name) 
+		})
+	} else {
+		spotify.search({ type: 'track', query: query }, function(error, data) {
+			if (error) {
+				return console.log('Error occurred: ' + err);
+			}
+
+			console.log("Artist: " + data.tracks.items[0].artists[0].name);
+			console.log("Song Name: " + data.tracks.items[0].name) 
+			console.log("Preview Link: " + data.tracks.items[0].album.external_urls.spotify)
+			console.log("Album: " + data.tracks.items[0].album.name) 
+		});
+	}
 }
 
 function movieThis(query) {
 
+	var omdbQueryUrl = "http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=40e9cece";
+	var defaultQueryUrl = "http://www.omdbapi.com/?t=Mr+Nobody&y=&plot=short&apikey=40e9cece";
+
 	request(omdbQueryUrl, function(error, response, body) {
 
-		if (!error && response.statusCode === 200) {
+		if (query === "") {
+			request(defaultQueryUrl, function(error, response, body) {
+				console.log("Title: " + JSON.parse(body).Title);
+				console.log("Release Year: " + JSON.parse(body).Year);
+				console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+				console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+				console.log("Country: " + JSON.parse(body).Country);
+				console.log("Language: " + JSON.parse(body).Language);
+				console.log("Plot: " + JSON.parse(body).Plot);
+				console.log("Actors: " + JSON.parse(body).Actors);
+			})
+		} else if (!error && response.statusCode === 200) {
 	    console.log("Title: " + JSON.parse(body).Title);
 	    console.log("Release Year: " + JSON.parse(body).Year);
-	    console.log("IMDB Rating: " + JSON.parse(body).Ratings);
-	    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings);
+	    console.log("IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+	    console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
 	    console.log("Country: " + JSON.parse(body).Country);
 	    console.log("Language: " + JSON.parse(body).Language);
 	    console.log("Plot: " + JSON.parse(body).Plot);
@@ -86,14 +108,7 @@ switch (action) {
 	break;
 }
 
-
-
-// if no song is provided, program defaults to "The Sign" by Ace of Base
-
 // check JSON.parse
 
-// fix ratings search for omdb... one array with objects inside
-
-// if user doesn't type in a movie, the program will default to Mr. Nobody
-
-// command "do-what-it-says" uses fs node package to take text inside of random.txt and then use it to call one of Liri's commands
+// command "do-what-it-says" uses fs node package 
+// to take text inside of random.txt and then use it to call one of Liri's commands
